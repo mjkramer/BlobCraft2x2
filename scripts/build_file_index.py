dddd18ed22abd3a801ea6f5a28507783f47bdd63
Matt Kramer <mjkramer@users.noreply.github.com>
2025-02-25 15:38:11 -0800

    Add --binary2packet to build_file_index.py

--- scripts/build_file_index.py
+++ scripts/build_file_index.py
@@ -9,19 +9,31 @@ def main():
     ap = argparse.ArgumentParser()
     ap.add_argument('-p', '--path',
                     help='Path to scan',
-                    required=True, type=Path)
+                    required=True)
     ap.add_argument('-e', '--ext',
                     help='File extension (e.g.: h5, hdf5, data)',
                     required=True)
     ap.add_argument('-o', '--output',
                     help='Pickle file in which to save the index dict',
-                    required=True, type=Path)
+                    required=True)
+    ap.add_argument('-b', '--binary2packet',
+                    help='convert charge paths from binary to packet',
+                    action='store_true')
+    ap.add_argument('-B', '--binary-dir',
+                    default='/global/cfs/cdirs/dune/www/data/FSD/CRS/CRS')
+    ap.add_argument('-P', '--packet-dir',
+                    default='/global/cfs/cdirs/dune/www/data/FSD/nearline/packet/CRS')
     args = ap.parse_args()
 
     result = {}
 
-    for p in args.path.rglob(f'*.{args.ext}'):
-        result[p.name] = str(p)
+    for p in Path(args.path).rglob(f'*.{args.ext}'):
+        name = p.name
+        if args.binary2packet:
+            p = Path(args.packet_dir) / p.relative_to(args.binary_dir)
+            if p.name.startswith('binary-'):
+                p = p.parent / p.name.replace('binary-', 'packet-', 1)
+        result[name] = str(p)
 
     with open(args.output, 'wb') as f:
         pickle.dump(result, f)
