7f5506f6d3e721488fcbe061cc2ff712c8d9f3d0
Matt Kramer <mjkramer@users.noreply.github.com>
2025-02-26 09:28:58 -0800

    Update examples

--- examples/make_2x2.sh
+++ examples/make_2x2.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+# ssh -L 8087:acd-daq05-priv:8086 -L 5461:ifdb11.fnal.gov:5461 acdcs@acd-gw05.fnal.gov
+
+outname=mx2x2runs_v0.2_beta1
+
 minrun=50000
 maxrun=50043
 
@@ -8,3 +12,10 @@ for runno in $(seq $minrun $maxrun); do
         scripts/make_db.sh "$runno"
     fi
 done
+
+# absorbed into 50005
+rm output/runs_50006.db
+
+scripts/merge_sqlite.py "$outname.sqlite" output/runs_*.db
+
+scripts/sqlite2excel.py -o "$outname.xlsx" -i "$outname.sqlite"
