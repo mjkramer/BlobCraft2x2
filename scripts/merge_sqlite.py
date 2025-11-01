eb5d10e5de6dfa000204836937ed0ad994a2803e
Matt Kramer <mjkramer@users.noreply.github.com>
2025-10-31 21:12:04 -0700

    Prevent merge_sqlite.py crash w/ partial 1st file

--- scripts/merge_sqlite.py
+++ scripts/merge_sqlite.py
@@ -7,6 +7,7 @@ import sqlite3
 def merge_sqlite(dest_file, source_files):
     dest_conn = sqlite3.connect(dest_file)
     dest_cursor = dest_conn.cursor()
+    created_tables = set()
 
     for isource, source_file in enumerate(source_files):
         source_conn = sqlite3.connect(source_file)
@@ -19,11 +20,11 @@ def merge_sqlite(dest_file, source_files):
             table_name = table[0]
             print(table_name)
 
-            if isource == 0:
-                # TODO: Make this not depend on the first file having everything
+            if table_name not in created_tables:
                 source_cursor.execute(f"SELECT sql FROM sqlite_master WHERE type='table' AND name='{table_name}';")
                 create_table_sql = source_cursor.fetchone()[0]
                 dest_cursor.execute(create_table_sql)
+                created_tables.add(table_name)
 
             pragma = f"PRAGMA table_info({table_name})"
             source_cursor.execute(pragma)
