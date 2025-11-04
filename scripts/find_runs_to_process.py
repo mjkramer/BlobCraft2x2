f0021f7e55e74a7578aba3c86e7ba4aa3840ba18
Matt Kramer <mjkramer@users.noreply.github.com>
2025-11-04 13:59:58 -0800

    Always process latest run (for now)

--- scripts/find_runs_to_process.py
+++ scripts/find_runs_to_process.py
@@ -10,21 +10,25 @@ def main():
     morcs_db='config/morcs.sqlite'
     # find list of runs from sqlite
     conn = sqlite3.connect(morcs_db)
-    cursor = conn.cursor()
-    cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
     df = pd.read_sql_query(f"SELECT * FROM {'run_data'}", conn)
     sqlite_runs = df.id.to_numpy()
     
     # look for runs already processed
     runs_processed = []
-    for file in os.listdir('output'):
+    for file in sorted(os.listdir('output')):
         if not file.endswith('.db'):
             continue
         match = re.search(r"runs_(\d+)\.db", file)
         if match:
             runnumber = int(match.group(1))
             runs_processed.append(runnumber)
-    
+
+    # Always reprocess the last processed run, in case of new subruns.
+    # FIXME: Use the end_date in morcs.sqlite to avoid this if not
+    # needed (first need to get MORCs to write the right end_date)
+    if runs_processed:
+        runs_processed.pop()
+
     runs_to_process = sqlite_runs[~np.isin(sqlite_runs, runs_processed)]
     print(f'Need to add the following runs to the DB: {runs_to_process}')
     
